8527cfc8f6f40d118abb3f223c0407a5e8968860
Alfred-Lau <jian.liu@icloud.com>
2021-03-13 14:53:12 +0800

    feat:添加 package 实体类 作为包功能的提供者

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -3,6 +3,10 @@
 module.exports = exec;
 
 function exec() {
-  // TODO
   console.log("exec", process.env.CLI_TARGET_PATH);
+
+  // 1， 拿到 targetPath ,转化为 modulePath
+  // 2. 实例化 Package 类
+  // 3. 获取入口文件  Package.getRootFile
+  // 4. 封装其他方法 到  Package 类上面
 }
---
+++ models/package/README.md
@@ -0,0 +1,11 @@
+# `@lerna-usage/package`
+
+> TODO: description
+
+## Usage
+
+```
+const package = require('@lerna-usage/package');
+
+// TODO: DEMONSTRATE API
+```
