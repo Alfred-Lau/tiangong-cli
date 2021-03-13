779eff05eb1f29e0b37cecb1006054a2bcadb41f
Alfred-Lau <jian.liu@icloud.com>
2021-03-13 14:27:47 +0800

    feat:添加全局调试的targetPath

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -5,5 +5,5 @@ module.exports = init;
 const log = require("@lerna-usage/log");
 
 function init(projectName, cmdObj) {
-  log.info("init", projectName, cmdObj);
+  log.info("init", projectName, cmdObj, process.env.CLI_TARGET_PATH);
 }
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,20 +7,18 @@ const semver = require("semver");
 const colors = require("colors/safe");
 const userHome = require("user-home");
 const exists = require("path-exists").sync;
+const commander = require("commander");
 const log = require("@lerna-usage/log");
 const { getLatestVersion } = require("@lerna-usage/get-npm-info");
-const minimist = require("minimist");
-const commander = require("commander");
-const pkg = require("../package.json");
 
-let args = process.argv.slice(2);
+const pkg = require("../package.json");
 
 // 第一种用法
 const program = new commander.Command();
 
 // 版本号检查功能
 function checkPkgVersion() {
-  // log.info("当前软件版本", pkg.version);
+  log.info("当前软件版本", pkg.version);
 }
 
 // 最低 node 版本兼容性检查
@@ -33,7 +31,7 @@ function checkNodeVersion() {
   if (semver.lt(currentNodeVersion, lastNodeVersion)) {
     throw new Error(colors.red(`node 版本必须高于 ${lastNodeVersion}`));
   } else {
-    // log.info("检查当前 Node 版本", colors.green(`${currentNodeVersion}`));
+    log.info("检查当前 Node 版本", colors.green(`${currentNodeVersion}`));
   }
 }
 
@@ -50,23 +48,8 @@ function checkUserHome() {
   if (!userHome || !exists(userHome)) {
     throw new Error(colors.red("当前用户的主目录不存在"));
   } else {
-    // log.info("检查用户主目录", ` ${userHome}`);
+    log.info("检查用户主目录", ` ${userHome}`);
   }
-}
-
-/**
- *检查输入参数, 添加debug 模式
- *
- */
-function checkUserInputArgs() {
-  args = minimist(args);
-  if (args.debug) {
-    process.env.LOG_LEVEL = "verbose";
-  } else {
-    process.env.LOG_LEVEL = "info";
-  }
-
-  log.level = process.env.LOG_LEVEL;
 }
 
 function checkDefaultEnv() {
@@ -80,8 +63,6 @@ function checkDefaultEnv() {
     });
   }
   creatDefaultEnvConfig();
-  // 后面直接使用这个就可以
-  log.verbose("当前环境变量配置: ", process.env.CLI_HOME_PATH);
 }
 
 function creatDefaultEnvConfig() {
@@ -110,12 +91,12 @@ async function checkGlobalUpdate() {
 
   const latestVersion = await getLatestVersion(name, currentVersion);
   if (semver.gt(latestVersion, currentVersion)) {
-    // log.info(
-    //   "检查最新版本",
-    //   `当前版本 ${currentVersion} 不是最新版本，请运行 ${colors.yellow(
-    //     `npm i -g ${name} `
-    //   )} 进行安装`
-    // );
+    log.info(
+      "检查最新版本",
+      `当前版本 ${currentVersion} 不是最新版本，请运行 ${colors.yellow(
+        `npm i -g ${name} `
+      )} 进行安装`
+    );
   }
 }
 
@@ -125,7 +106,8 @@ function registryCommand() {
     .name(Object.keys(pkg.bin)[0])
     .usage("<command> [option]")
     .version(pkg.version)
-    .option("-d, --debug", "是否开启调试模式", false);
+    .option("-d, --debug", "是否开启调试模式", false)
+    .option("-tp, --targetPath <targetPath>", "设置目标调试路径", "");
 
   // third 正式开始注册事件
   program
@@ -146,6 +128,13 @@ function registryCommand() {
 
     log.level = process.env.LOG_LEVEL;
     log.verbose("我是在进行 debug");
+  });
+
+  program.on("option:targetPath", function () {
+    const target_path = this.opts().targetPath;
+    if (target_path) {
+      process.env.CLI_TARGET_PATH = target_path;
+    }
   });
 
   // 未知命令的处理逻辑
@@ -170,18 +159,19 @@ function registryCommand() {
   program.parse(process.argv);
 }
 
+async function prepare() {
+  checkPkgVersion();
+  checkNodeVersion();
+  checkRoot();
+  checkUserHome();
+  checkDefaultEnv();
+  await checkGlobalUpdate();
+}
+
 async function cli(argv) {
   try {
-    checkPkgVersion();
-    checkNodeVersion();
-    checkRoot();
-    checkUserHome();
-    // checkUserInputArgs();
-    checkDefaultEnv();
-    await checkGlobalUpdate();
     //  完成命令注册
     registryCommand();
-    // tg-cli --debug 生效
   } catch (error) {
     // 隐藏堆栈信息，自定义
     log.error(error.message);
