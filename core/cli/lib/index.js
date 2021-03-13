8beba4aa00a33e17f5bbc6849d42e4ebe45dafba
Alfred-Lau <jian.liu@icloud.com>
2021-03-13 12:24:32 +0800

    feat:添加init 命令注册入口

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -6,17 +6,21 @@ const path = require("path");
 const semver = require("semver");
 const colors = require("colors/safe");
 const userHome = require("user-home");
-const exists = require("path-exists");
+const exists = require("path-exists").sync;
 const log = require("@lerna-usage/log");
 const { getLatestVersion } = require("@lerna-usage/get-npm-info");
 const minimist = require("minimist");
+const commander = require("commander");
 const pkg = require("../package.json");
 
 let args = process.argv.slice(2);
 
+// 第一种用法
+const program = new commander.Command();
+
 // 版本号检查功能
 function checkPkgVersion() {
-  log.info("当前软件版本", pkg.version);
+  // log.info("当前软件版本", pkg.version);
 }
 
 // 最低 node 版本兼容性检查
@@ -29,7 +33,7 @@ function checkNodeVersion() {
   if (semver.lt(currentNodeVersion, lastNodeVersion)) {
     throw new Error(colors.red(`node 版本必须高于 ${lastNodeVersion}`));
   } else {
-    log.info("检查当前 Node 版本", colors.green(`${currentNodeVersion}`));
+    // log.info("检查当前 Node 版本", colors.green(`${currentNodeVersion}`));
   }
 }
 
@@ -46,7 +50,7 @@ function checkUserHome() {
   if (!userHome || !exists(userHome)) {
     throw new Error(colors.red("当前用户的主目录不存在"));
   } else {
-    log.info("检查用户主目录", ` ${userHome}`);
+    // log.info("检查用户主目录", ` ${userHome}`);
   }
 }
 
@@ -99,20 +103,71 @@ function creatDefaultEnvConfig() {
  *检查最新版本
  *
  */
-async function checkLatestVersion() {
+async function checkGlobalUpdate() {
   const currentVersion = pkg.version;
   // const name = pkg.name;
   const name = "colors";
 
   const latestVersion = await getLatestVersion(name, currentVersion);
   if (semver.gt(latestVersion, currentVersion)) {
-    log.info(
-      "检查最新版本",
-      `当前版本 ${currentVersion} 不是最新版本，请运行 ${colors.yellow(
-        `npm i -g ${name} `
-      )} 进行安装`
-    );
+    // log.info(
+    //   "检查最新版本",
+    //   `当前版本 ${currentVersion} 不是最新版本，请运行 ${colors.yellow(
+    //     `npm i -g ${name} `
+    //   )} 进行安装`
+    // );
   }
+}
+
+function registryCommand() {
+  // first
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [option]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false);
+
+  // third 正式开始注册事件
+  program
+    .command("init [name]")
+    .option("-f, --force", "是否强制初始化项目")
+    .action((projectName, cmdObj) => {
+      log.info("init", projectName, cmdObj);
+    });
+
+  // second 设置事件监听
+  program.on("option:debug", function () {
+    // 重点：发现和书上不太一样的时候，要寻根溯源到文档，更新的用法
+    if (this.opts().debug) {
+      process.env.LOG_LEVEL = "verbose";
+    } else {
+      process.env.LOG_LEVEL = "info";
+    }
+
+    log.level = process.env.LOG_LEVEL;
+    log.verbose("我是在进行 debug");
+  });
+
+  // 未知命令的处理逻辑
+  program.on("command:*", function (unkonwnCommand) {
+    const availableCommands = program.commands.map((cmd) => cmd.name());
+    log.warn(colors.red(`${unkonwnCommand} 命令不存在`));
+    program.outputHelp();
+    if (availableCommands.length) {
+      console.log(
+        colors.yellow(`可用的命令有: ${availableCommands.join(",")}`)
+      );
+    }
+  });
+
+  // node exe.js xxx
+  if (process.argv.length < 3) {
+    program.outputHelp();
+    console.log();
+  }
+
+  // last
+  program.parse(process.argv);
 }
 
 async function cli(argv) {
@@ -121,9 +176,11 @@ async function cli(argv) {
     checkNodeVersion();
     checkRoot();
     checkUserHome();
-    checkUserInputArgs();
+    // checkUserInputArgs();
     checkDefaultEnv();
-    await checkLatestVersion();
+    await checkGlobalUpdate();
+    //  完成命令注册
+    registryCommand();
     // tg-cli --debug 生效
   } catch (error) {
     // 隐藏堆栈信息，自定义
