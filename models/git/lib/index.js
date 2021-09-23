fbe56a39d15cf4a2e489dbcd2879eb9e12fba081
Alfred-Lau <jian.liu@icloud.com>
2021-09-23 23:32:49 +0800

    feat(git): add checkHomePath

--- models/git/lib/index.js
+++ models/git/lib/index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const simpleGit = require("simple-git");
+const path = require("path");
+const userHome = require("user-home");
+const fse = require("fs-extra");
 const log = require("@tiangongkit/log");
 
+const DEFAULT_CLI_HOME = ".tg_cli";
 class Git {
   constructor({ name, version, dir }) {
     this.name = name;
@@ -17,10 +21,21 @@ class Git {
   }
 
   checkHomePath() {
-    log.info("", "开始检查用户目录是否存在");
+    if (!this.homePath) {
+      if (process.env.CLI_HOME_PATH) {
+        this.homePath = process.env.CLI_HOME_PATH;
+      } else {
+        this.homePath = path.resolve(userHome, DEFAULT_CLI_HOME);
+      }
+    }
+    fse.ensureDirSync(this.homePath);
+    if (!fse.existsSync(this.homePath)) {
+      throw new Error("用户主目录获取失败");
+    }
   }
   init(options) {
     log.info("git init", options);
+    this.prepare();
   }
 }
 module.exports = Git;
